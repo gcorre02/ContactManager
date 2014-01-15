5dc57249600c48306a78588496eafc2be418341e
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-01-15 02:27:07 +0000

    Added impl test and stubs for Meeting interface

---
+++ ContactManager/src/contactmgmt/MeetingImpl.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package contactmgmt;
+
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * @author Guilherme
+ *
+ */
+public class MeetingImpl implements Meeting {
+
+	/**
+	 * 
+	 */
+	public MeetingImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Meeting#getId()
+	 */
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Meeting#getDate()
+	 */
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Meeting#getContacts()
+	 */
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ContactManager/test/test/MeetingImplTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Guilherme
+ *
+ */
+public class MeetingImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.MeetingImpl#MeetingImpl()}.
+	 */
+	@Test
+	public final void testMeetingImpl() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.MeetingImpl#getId()}.
+	 */
+	@Test
+	public final void testGetId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.MeetingImpl#getDate()}.
+	 */
+	@Test
+	public final void testGetDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.MeetingImpl#getContacts()}.
+	 */
+	@Test
+	public final void testGetContacts() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
