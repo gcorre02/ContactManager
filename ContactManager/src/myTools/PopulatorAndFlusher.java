e31c540fe627c9ee673999e5cb834fc3e6ce54dd
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-01-15 22:45:30 +0000

    wrote interface for the indexSetUpnFlusher class ( doesnt have this name )

--- ContactManager/src/myTools/PopulatorAndFlusher.java
+++ ContactManager/src/myTools/PopulatorAndFlusher.java
@@ -3,10 +3,37 @@
  */
 package myTools;
 
+import java.util.Set;
+
+import contactmgmt.Contact;
+import contactmgmt.Meeting;
+
 /**
  * @author Guilherme
  *
  */
 public interface PopulatorAndFlusher {
+	/* 
+	 * will need all getters and setters for the private fields at impl 
+	 * 
+	 * PopulateSetsAndIndexes(String pathToFile), for each list/set = PopulateSetsandIndexes.gettersAndSetters()
+	 * 
+	 * WriteToFile(Meetings and COntacts sets overriden, pathToFile) *2
+	 */
+	/**
+	 * called at set-up by ContactManager
+	 * populates all Sets and Indexes
+	 * @param pathToFile
+	 * 
+	 */
+	void PopulateSetsAndIndexes(String pathToFile);
+
+	/**
+	 * 
+	 * @param allMeetings set of all meetings to be written to the file
+	 * @param allContacts set of all contacts to be written to the file
+	 * @param pathToFile location of the file
+	 */
+	void  WriteToFile(Set<Meeting> allMeetings, Set<Contact> allContacts, String pathToFile);
 
 }
