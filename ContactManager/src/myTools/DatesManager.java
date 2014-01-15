faa54b7da08ce41da1bc1d10ef0a1a5d5a01815b
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-01-15 22:38:00 +0000

    Wrote interface for the Dates manager

--- ContactManager/src/myTools/DatesManager.java
+++ ContactManager/src/myTools/DatesManager.java
@@ -3,10 +3,36 @@
  */
 package myTools;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * @author Guilherme
  *
  */
 public interface DatesManager {
+
+	/*
+	 * checkDateIsInThePast(Calendar date)	
+	 *
+	 * generateCalendarItem() < TODO consider adding a range in time, not just a date// later
+	 * 
+	 */
+	
+	/**
+	 * checks date is in the past
+	 * @param date the date being checked
+	 * @return true if date is in the past
+	 */
+	boolean checkDateIsInThePast(Calendar date);
+	
+	/**
+	 * 
+	 * @param year the year to add to the Calendar object
+	 * @param month the month to add to the Calendar object
+	 * @param day the day to add to the Calendar object
+	 * @return a date(GregorianCalendar) object
+	 */
+	GregorianCalendar generateCalendarItem(int year, int month, int day);
 
 }
