1c366cf4443de32a90db10f3e4c4ce67852520e6
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-01-15 01:23:17 +0000

    Stubbed ContactImpl and it's tests

---
+++ ContactManager/src/contactmgmt/ContactImpl.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package contactmgmt;
+
+/**
+ * @author Guilherme
+ *
+ */
+public class ContactImpl implements Contact {
+
+	/**
+	 * 
+	 */
+	public ContactImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Contact#getId()
+	 */
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Contact#getName()
+	 */
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Contact#getNotes()
+	 */
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see contactmgmt.Contact#addNotes(java.lang.String)
+	 */
+	@Override
+	public void addNotes(String note) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ContactManager/test/test/ContactImplTest.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Guilherme
+ *
+ */
+public class ContactImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.ContactImpl#ContactImpl()}.
+	 */
+	@Test
+	public final void testContactImpl() {
+		
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.ContactImpl#getId()}.
+	 */
+	@Test
+	public final void testGetId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.ContactImpl#getName()}.
+	 */
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.ContactImpl#getNotes()}.
+	 */
+	@Test
+	public final void testGetNotes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link contactmgmt.ContactImpl#addNotes(java.lang.String)}.
+	 */
+	@Test
+	public final void testAddNotes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
