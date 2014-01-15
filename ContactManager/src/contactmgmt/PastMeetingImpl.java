5a26cc784b85703feee66129c42e9230bdb3ddda
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-01-15 11:18:23 +0000

    removed the synching of any .* file (eclipse structure files) and added a new constructor to pastMeeting that allows for no input of notes(returning empty "")

--- ContactManager/src/contactmgmt/PastMeetingImpl.java
+++ ContactManager/src/contactmgmt/PastMeetingImpl.java
@@ -18,11 +18,22 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting{
 	 * @param contacts 
 	 * @param date 
 	 * @param inputId 
-	 * 
+	 * @param notes
 	 */
 	public PastMeetingImpl(int inputId, Calendar date, Set<Contact> contacts, String notes) {
 		super(inputId, date, contacts);
 		this.notes = notes;
+	}
+	
+	/**
+	 * @param contacts 
+	 * @param date 
+	 * @param inputId 
+	 * 
+	 */
+	public PastMeetingImpl(int inputId, Calendar date, Set<Contact> contacts) {
+		super(inputId, date, contacts);
+		this.notes = "";
 	}
 
 	/* (non-Javadoc)
