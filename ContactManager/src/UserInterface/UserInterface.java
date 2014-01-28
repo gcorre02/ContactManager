8dbee6bd9465b24b66504d2967568889820f3db9
Guilherme Ribeiro <guilherme.ctr@gmail.com>
2014-01-28 17:06:23 +0000

    Eclipse reconfig, safe

---
+++ ContactManager/src/UserInterface/UserInterface.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package UserInterface;
+
+/**
+ * @author Guilherme
+ *
+ */
+public interface UserInterface {
+	/**
+	 * Prompts user for contact management
+	 */
+	void promptUser();
+	
+}
